bb2027241b372e93e858e7e5a9eeb4f228bd5eab
geof.hoffman <geoff.c.137@gmail.com>
2020-12-19 23:17:51 -0500

    first commit

---
+++ popup.js
@@ -0,0 +1,24 @@
+$(function(){
+
+    $('#name').keyup(function(){
+        $('#greet').text('Hello,' + $('#name').val());
+    })
+
+
+
+const input = document.querySelector('input');
+const log = document.getElementById('values');
+
+input.addEventListener('input', updateValue);
+
+function updateValue(e) {
+  log.textContent = e.target.value;
+}
+
+$(document).ready(function(){
+    $('input[type="checkbox"]').click(function(){
+        var inputValue = $(this).attr("value");
+        $("." + inputValue).toggle();
+    });
+});
+});
